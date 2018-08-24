36285f0610baf5b6bd941dd9a911392ab903b24e
Ömer Sinan Ağacan <omeragacan@gmail.com>
2018-08-24 17:59:07 +0300

    Fix merging

--- src/ExternalSort.hs
+++ src/ExternalSort.hs
@@ -9,15 +9,18 @@ module ExternalSort where
   ) where
   -}
 
-import Control.Monad (forM, forM_)
+import Control.Monad
 import qualified Data.Binary as B
 import qualified Data.Binary.Get as B
 import qualified Data.Binary.Put as B
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
-import Data.List (sort)
+import Data.List (sort, minimumBy)
 import System.FilePath ((</>))
 import System.IO
+import Data.Ord
+import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector as V
 
 type Get handle a = handle -> IO (Maybe a, handle)
 type Put handle a = handle -> a -> IO ()
@@ -94,43 +97,130 @@ sortExternal chunk_size tmp_dir file_in get put mk_get mk_put close_get close_pu
 
     -- Read chunk_size/(n_chunks + 1) sized chunks from each chunk, do
     -- n_chunks-way merge sort, write result to the output file.
-    let
-      sort_chunk_size = max (chunk_size `div` (n_chunks + 1)) 1
-      sort_steps = ceiling (fromIntegral chunk_size / fromIntegral (n_chunks + 1) :: Double) :: Int
+    let sort_chunk_size = max (chunk_size `div` (n_chunks + 1)) 1
 
     -- TODO: These handles will leak on exception
     -- TODO: close handles
     hs <- forM [0..n_chunks-1] (\n_chunk -> mk_get (tmp_dir </> show n_chunk))
     h_out <- mk_put file_out
 
-    sort_loop sort_steps sort_chunk_size hs h_out
+    merge' (zip (repeat get) hs) close_get sort_chunk_size (put, h_out)
+    close_put h_out
+
+
+--------------------------------------------------------------------------------
+-- N-way merge. Load N chunks into memory, merge pair-wise. When a list is
+-- completely merged, load more elements. If there are no more elements for that
+-- list drop it.
+
+data MergeList get_handle a = MergeList
+    { _mlList   :: [a]
+        -- ^ Elements of the list in memory.
+    , _mlHandle :: get_handle
+        -- ^ Handle to read more elements.
+    , _mlEOF    :: Bool
+        -- ^ Reached eof?
+    , _mlGet    :: Get get_handle a
+        -- ^ Use to get more elements.
+    }
+
+peekML :: Int -> MergeList get_handle a -> IO (Maybe a, MergeList get_handle a)
+peekML chunk_size (MergeList lst handle eof get)
+  | h : _ <- lst
+  = return (Just h, MergeList lst handle eof get)
+
+  | not eof
+  = do (lst', handle', eof') <- readChunk get handle chunk_size
+       if null lst'
+         then return (Nothing, MergeList [] handle eof' get)
+         else peekML chunk_size (MergeList lst' handle' eof' get)
+
+  | otherwise
+  = return (Nothing, MergeList [] handle True get)
+
+unconsML :: MergeList get_handle a -> (a, MergeList get_handle a)
+unconsML ml =
+    case _mlList ml of
+      [] -> error "unconsML: Empty list. Make sure you call peekML first."
+      h : t -> (h, ml{ _mlList = t })
+
+consML :: a -> MergeList get_handle a -> MergeList get_handle a
+consML a l = l{ _mlList = a : _mlList l }
+
+merge'
+    :: forall get_handle put_handle a . Ord a
+    => [(Get get_handle a, get_handle)]
+    -> (get_handle -> IO ())
+    -> Int -- ^ Chunk size
+    -> (Put put_handle a, put_handle)
+    -> IO ()
+
+merge' ins close_in chunk_size0 (put, put_handle) = do
+    -- A vector of lists to merge. Consumed lists are replaced with `Nothing`
+    -- and moved to the end of the list.
+    mls <- V.thaw (V.fromList (map mk_ml ins))
+
+    go chunk_size0 mls
   where
-    sort_loop 0 _ hs h_out = do
-      forM_ hs close_get
-      close_put h_out
+    mk_ml (get, get_handle) = MergeList [] get_handle False get
 
-    sort_loop n sort_chunk_size hs h_out = do
-      -- TODO: Drop files that return Nothing for the next iteration
-      (sorted_chunks, hs') <-
-        fmap unzip $
-        forM hs (\h -> fmap (\(is, h', _) -> (is, h'))
-                            (readChunk get h sort_chunk_size))
+    go :: Int -- ^ Chunk size. Update as lists consumed.
+       -> MV.IOVector (MergeList get_handle a)
+              -- ^ Lists to merge
+       -> IO ()
 
-      forM_ (merge sorted_chunks) (put h_out)
+    go _ mls
+      | MV.null mls = return ()
 
-      sort_loop (n - 1) sort_chunk_size hs' h_out
+    go chunk_size mls = do
 
--- | Simple N-way merge
-merge :: Ord a => [[a]] -> [a]
-merge = foldr merge2 []
+      putStrLn ("Looping. Chunk size: " ++ show chunk_size)
 
--- | 2-way merge
-merge2 :: Ord a => [a] -> [a] -> [a]
-merge2 l1@(i1 : is1) l2@(i2 : is2)
-  | i1 < i2   = i1 : merge2 is1 l2
-  | otherwise = i2 : merge2 l1 is2
-merge2 l1 [] = l1
-merge2 [] l2 = l2
+      (mls', heads) <- read_heads chunk_size mls 0 []
+
+      unless (null heads) $ do
+        let (min_idx, min_) = minimumBy (comparing snd) heads
+
+        min_l <- MV.read mls min_idx
+        MV.write mls min_idx (snd (unconsML min_l))
+
+        put put_handle min_
+
+        go (max {- (chunk_size0 `div` MV.length mls') -} chunk_size0 1) mls'
+
+    read_heads
+      :: Int
+      -> MV.IOVector (MergeList get_handle a)
+      -> Int
+      -> [(Int, a)]
+      -> IO (MV.IOVector (MergeList get_handle a), [(Int, a)])
+
+    read_heads _ v i acc
+      | i == MV.length v
+      = return (v, acc)
+
+    read_heads chunk_size v i acc = do
+      (mb_a, l) <-
+        MV.read v i >>= peekML chunk_size
+
+      MV.write v i l
+
+      case mb_a of
+        Nothing -> do
+          -- Close the handle, move it to the end of the vector, shrink the
+          -- vector
+          close_in (_mlHandle l)
+          if (i == MV.length v - 1) then do
+            -- Last element
+            return (MV.slice 0 (MV.length v - 1) v, acc)
+          else do
+            MV.swap v i (MV.length v - 1)
+            read_heads chunk_size (MV.slice 0 (MV.length v - 1) v) i acc
+        Just a ->
+          read_heads chunk_size v (i + 1) ((i, a) : acc)
+
+--------------------------------------------------------------------------------
+
 
 -- | Split the file into `chunk_size`-sized sorted chunks. Put chunks in the `tmp_dir`.
 generateChunks
